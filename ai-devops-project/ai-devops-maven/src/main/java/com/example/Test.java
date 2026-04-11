17722d68ec119378dbe70200b7079dbd4c27579b
unknown <you@example.com>
2026-04-12 00:52:37 +0530

    AI auto-fix commit [skip ci]

--- ai-devops-project/ai-devops-maven/src/main/java/com/example/Test.java
+++ ai-devops-project/ai-devops-maven/src/main/java/com/example/Test.java
@@ -3,8 +3,8 @@ package com.example;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello")
 
-        int a = 10;
+        int a = 10
     }
 }
